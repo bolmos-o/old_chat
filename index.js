dd4bc962d1e65e48f3eee7d4dd1709525fd812b7
Bruno OLMOS OSORIO <bolmos-o@e2r1p13.clusters.42paris.fr>
2022-02-01 21:44:49 +0100

    adding express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send("hello"));
+
+app.listen(port, () => console.log(`listen on ${port}`));
